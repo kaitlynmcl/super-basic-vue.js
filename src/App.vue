b172afd3e49e286d2b55f0191ef46549e0446b8a
Kaitlyn McLaughlin <kaitlyn@Kaitlyns-MacBook-Pro.local>
2021-01-26 10:23:56 -0500

    add components?

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,22 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <First msg="First Component" /> 
+    <Users />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import First from './components/First.vue'
+import Users from './components/Users.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    // First,
+    Users
   }
 }
 </script>
